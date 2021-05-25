2576b41e62c429a3492fb81716ccad7125192715
aditya thakkar <aditya.upwork@mindcrewtech.com>
2021-05-25 12:44:24 +0530

    WeatherApp
    WeatherApp 25 may 2021

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import ListScreen from './component/ListScreen';
+import MapsScreen from './component/MapsScreen';
+import Splash from './component/splash'
+import { ContextProvider } from './context'
+const RootStack = createStackNavigator(
+  {
+    Splash: Splash,
+    List: ListScreen,
+    Maps: MapsScreen,
+  },
+  {
+    initialRouteName: 'Splash',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#19AC52',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+const RootContainer = createAppContainer(RootStack);
+export default function App() {
+  return (
+    <ContextProvider>
+      <RootContainer />
+    </ContextProvider>)
+}
